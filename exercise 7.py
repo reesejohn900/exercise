7d007fb8258a1148fab45933a14843fbc5a387e2
reesejohn900 <68529800+reesejohn900@users.noreply.github.com>
2020-07-21 11:06:11 +0530

    Update exercise 7.py

--- exercise 7.py
+++ exercise 7.py
@@ -10,39 +10,49 @@ def getdate():
     return datetime.datetime.now()
 
 def water():
-    pygame.mixer.music.load('water.mp3')
-    pygame.mixer.music.play()
-
-    f_1=open('water.txt','a')
-    a=input()
-    f_1.write(str(str(getdate()))+': '+a+'\n')
-    pygame.mixer.music.stop()
+    while True:
+        pygame.mixer.music.load('water.mp3')
+        pygame.mixer.music.play()
+        a=input('press drank to stop')
+        if a=='drank':
+            f_1=open('water.txt','a')
+        
+            f_1.write(str(str(getdate()))+': '+a+'\n')
+            pygame.mixer.music.stop()
+            break
 
 def eyes():
-    pygame.mixer.music.load('eyes.mp3')
-    pygame.mixer.music.play()
-
-    f_2 = open('eyes.txt', 'a')
-    b = input()
-    f_2.write(str(str(getdate())) + ': ' + b + '\n')
-    pygame.mixer.music.stop()
-
+    while True:
+        pygame.mixer.music.load('eyes.mp3')
+        pygame.mixer.music.play()
+        b = input('press done to stop')
+        if b == 'done'
+            f_2 = open('eyes.txt', 'a')
+    
+            f_2.write(str(str(getdate())) + ': ' + b + '\n')
+            pygame.mixer.music.stop()
+            break
+            
 def physical_exercise():
-    pygame.mixer.music.load('physical_exercise.mp3')
-    pygame.mixer.music.play()
+    while True:
+        pygame.mixer.music.load('physical_exercise.mp3')
+        pygame.mixer.music.play()
+        c == 'input('press done to stop')
+        if c == 'done'
+            f_3 = open('physical_exercise.txt', 'a')
+    
+            f_3.write(str(str(getdate())) + ': ' + c + '\n')
+            pygame.mixer.music.stop()
+            break
+if __name__ == '__main__':            
 
-    f_3 = open('physical_exercise.txt', 'a')
-    c = input()
-    f_3.write(str(str(getdate())) + ': ' + c + '\n')
-    pygame.mixer.music.stop()
+    schedule.every(5).seconds.do(water)
 
-schedule.every(5).seconds.do(water)
+    schedule.every(8).seconds.do(eyes)
 
-schedule.every(8).seconds.do(eyes)
+    schedule.every(11).seconds.do(physical_exercise)
 
-schedule.every(11).seconds.do(physical_exercise)
+    while(True):
+        schedule.run_pending()
 
-while(True):
-    schedule.run_pending()
-
-    time.sleep(2)
+        time.sleep(2)
