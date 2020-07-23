5726f22771e14e79e95df190aeb4882d822a2c36
reesejohn900 <68529800+reesejohn900@users.noreply.github.com>
2020-07-23 12:25:58 +0530

    Update exercise 7.py

--- exercise 7.py
+++ exercise 7.py
@@ -6,45 +6,83 @@ import schedule
 pygame.init()
 pygame.mixer.init()
 
+
+
 def getdate():
     return datetime.datetime.now()
 
+
 def water():
-    while True:
-        pygame.mixer.music.load('water.mp3')
-        pygame.mixer.music.play()
-        a=input('press drank to stop')
-        if a=='drank':
-            f_1=open('water.txt','a')
+    pygame.mixer.music.load('water.mp3')
+    pygame.mixer.music.play(-1)
         
-            f_1.write(str(str(getdate()))+': '+a+'\n')
-            pygame.mixer.music.stop()
-            break
+
+    a = input('type drank to stop\n')
+
+    if not a :
+        pygame.mixer.music.stop()
+
+
+
+    elif a=='drank':
+        pygame.mixer.music.stop()
+
+
+        f_1=open('water.txt','a')
+        
+        f_1.write(str(str(getdate()))+': '+a+'\n')
+
+
+
+
+
 
 def eyes():
-    while True:
-        pygame.mixer.music.load('eyes.mp3')
-        pygame.mixer.music.play()
-        b = input('press done to stop')
-        if b == 'done'
-            f_2 = open('eyes.txt', 'a')
+
+    pygame.mixer.music.load('eyes.mp3')
+    pygame.mixer.music.play(-1)
+    b = input('type done to stop\n')
+    if not b:
+
+        pygame.mixer.music.stop()
+
+
+
+    elif b== 'done':
+        pygame.mixer.music.stop()
+
+        f_2 = open('eyes.txt', 'a')
     
-            f_2.write(str(str(getdate())) + ': ' + b + '\n')
-            pygame.mixer.music.stop()
-            break
+        f_2.write(str(str(getdate())) + ': ' + b + '\n')
+
+
+            
             
 def physical_exercise():
-    while True:
-        pygame.mixer.music.load('physical_exercise.mp3')
-        pygame.mixer.music.play()
-        c == 'input('press done to stop')
-        if c == 'done'
-            f_3 = open('physical_exercise.txt', 'a')
+
+    pygame.mixer.music.load('exercise.mp3')
+    pygame.mixer.music.play(-1)
+    c = input('type did to stop')
+    if not c:
+
+        pygame.mixer.music.stop()
+
+
+
+    elif c== 'did':
+        pygame.mixer.music.stop()
+
+        f_3 = open('physical_exercise.txt', 'a')
     
-            f_3.write(str(str(getdate())) + ': ' + c + '\n')
-            pygame.mixer.music.stop()
-            break
-if __name__ == '__main__':            
+        f_3.write(str(str(getdate())) + ': ' + c + '\n')
+
+
+
+
+
+
+if __name__ == '__main__':           
+
 
     schedule.every(5).seconds.do(water)
 
@@ -52,7 +90,7 @@ if __name__ == '__main__':
 
     schedule.every(11).seconds.do(physical_exercise)
 
-    while(True):
+    while True:
         schedule.run_pending()
 
         time.sleep(2)
